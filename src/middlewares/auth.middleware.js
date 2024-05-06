e60554a02bd180400aa47df75e26184b3ef2f943
abdul-haseeb123 <syedabdulhaseeb611@gmail.com>
2024-05-06 18:48:45 +0500

    created auth middleware, added auth middleware in logout route

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,34 @@
+import { User } from "../models/user.model.js";
+import { asyncHandler } from "../utils/index.js";
+import jwt from "jsonwebtoken";
+import { ApiError } from "../utils/ApiError.js";
+
+export const authMiddleware = asyncHandler(async (req, _, next) => {
+  try {
+    const accessToken =
+      req.cookies.accessToken ||
+      req.headers.authorization.replace("Bearer ", "");
+
+    if (!accessToken) {
+      throw new ApiError(401, "Unauthorized");
+    }
+
+    const decodedToken = jwt.verify(
+      accessToken,
+      process.env.ACCESS_TOKEN_SECRET
+    );
+
+    const user = await User.findById(decodedToken._id).select(
+      "-refreshToken -password"
+    );
+
+    if (!user) {
+      throw new ApiError(401, "Invalid access token");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, "Invalid access token");
+  }
+});
