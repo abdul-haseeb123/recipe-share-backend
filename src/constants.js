dcf6ae797adf03542e918702296a13f1f63c8067
abdul-haseeb123 <syedabdulhaseeb611@gmail.com>
2024-05-11 15:35:56 +0500

    feat: Add new recipe categories
    The code changes in this commit add new recipe categories to the constants file. This allows for better organization and categorization of recipes, enhancing the user experience and making it easier to browse and search for specific types of recipes.

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,16 @@
 export const DB_NAME = "recipes";
+
+export const CATEGORIES = [
+  "Breakfast recipes",
+  "Lunch recipes",
+  "Dinner recipes",
+  "Appetizer recipes",
+  "Salad recipes",
+  "Main-course recipes",
+  "Side-dish recipes",
+  "Baked-good recipes",
+  "Dessert recipes",
+  "Snack recipes",
+  "Soup recipes",
+  "Vegetarian recipes",
+];
