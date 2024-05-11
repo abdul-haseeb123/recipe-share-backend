2a8d0b8d39959c3b6f587ff13efdeeafc50a5ac1
abdul-haseeb123 <syedabdulhaseeb611@gmail.com>
2024-05-11 19:08:35 +0500

    feat: Add validation for comment and rating in createReview controller

--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -11,6 +11,14 @@ const createReview = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Comment and rating are required");
   }
 
+  if (rating < 1 || rating > 5) {
+    throw new ApiError(400, "Rating must be between 1 and 5");
+  }
+
+  if (comment.trim() === "") {
+    throw new ApiError(400, "Comment is required");
+  }
+
   const { recipeSlug } = req.params;
   const recipe = await Recipe.findOne({ slug: recipeSlug });
   if (!recipe) {
@@ -37,4 +45,59 @@ const createReview = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, review, "Review created successfully"));
 });
 
-export { createReview };
+const updateReview = asyncHandler(async (req, res) => {
+  const { comment, rating, reviewId } = req.body;
+
+  if (!comment || !rating || !reviewId) {
+    throw new ApiError(400, "Comment and rating are required");
+  }
+
+  if (rating < 1 || rating > 5) {
+    throw new ApiError(400, "Rating must be between 1 and 5");
+  }
+
+  if (comment.trim() === "") {
+    throw new ApiError(400, "Comment is required");
+  }
+
+  const review = await Review.findById(reviewId);
+
+  if (!review) throw new ApiError(404, "Review not found");
+
+  review.comment = comment;
+  review.rating = rating;
+
+  await review.save({ validateBeforeSave: false });
+
+  res.status(200).json(new ApiResponse(200, {}, "Review updated successfully"));
+});
+
+const deleteReview = asyncHandler(async (req, res) => {
+  const { reviewId } = req.body;
+  const { recipeSlug } = req.params;
+
+  if (!reviewId) {
+    throw new ApiError(400, "Review ID is required");
+  }
+
+  const recipe = await Recipe.findOne({ slug: recipeSlug });
+  if (!recipe) {
+    throw new ApiError(404, "Recipe not found");
+  }
+
+  const review = await Review.findById(reviewId);
+
+  if (!review) {
+    throw new ApiError(404, "Review not found");
+  }
+
+  await Review.findByIdAndDelete(reviewId);
+
+  const index = recipe.reviews.indexOf(reviewId);
+  recipe.reviews.splice(index, 1);
+  await recipe.save({ validateBeforeSave: false });
+
+  res.status(200).json(new ApiResponse(200, {}, "Review deleted successfully"));
+});
+
+export { createReview, updateReview, deleteReview };
