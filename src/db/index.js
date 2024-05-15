1fb602ae42ce7cf6eb32f7618b95381c04dfa721
abdul-haseeb123 <syedabdulhaseeb611@gmail.com>
2024-05-15 11:46:20 +0500

    feat: Add Dockerfile and docker-compose files for containerization

--- src/db/index.js
+++ src/db/index.js
@@ -4,10 +4,7 @@ import { DB_NAME } from "../constants.js";
 export const connectDb = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`,
-      {
-        authSource: "admin",
-      }
+      `${process.env.MONGODB_URI}/${DB_NAME}`
     );
     console.log(
       "Connected to the MONGODB: ",
